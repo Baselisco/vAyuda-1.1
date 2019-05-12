7ed155731997a6fa1491e715432d2395e8dc5803
Baselisco <50551173+Baselisco@users.noreply.github.com>
2019-05-12 17:50:04 +0200

    Add files via upload

---
+++ help-c.lua
@@ -0,0 +1,8 @@
+RegisterCommand("ayuda", function()
+	msg("Discord: https://discord.gg/yStJKZy")
+	msg("Página: www.europeanrp.com")
+end, false)
+
+function msg(text)
+	TriggerEvent("chatMessage", "Anuncios", {255,0,0}, tex}
+end
